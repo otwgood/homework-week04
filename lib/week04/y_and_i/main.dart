6abffd6bdef02d53485fdd070e4362b74f6e3142
otwgood <otwgood@naver.com>
2025-09-28 15:41:21 +0900

    .

--- lib/week04/y_and_i/main.dart
+++ lib/week04/y_and_i/main.dart
@@ -1,8 +1,37 @@
+import 'dart:math';
+
 import 'package:myapp/week04/u_and_i/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    materialApp
-  )
+    MaterialApp(
+      theme: ThemeDate(
+        fontFamily: 'sunflower',
+        textTheme: TextTheme(
+          headlineLarge: TextStyle (
+            color: Colors.white,
+            fontSize: 80.0,
+            fontWeight: FontWeight.w700m
+            fontFamily: 'parisienne',
+          ),
+          headlineMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 50.0,
+            fontWeight: FontWeight.w700,
+          ),
+          bodyMedium: TextStyle (
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+          bodySmall: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        )
+      ),
+      
+      home: HomeScreen(),
+    ),
+  );
 }
