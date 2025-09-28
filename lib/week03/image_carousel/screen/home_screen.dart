2bd99b92892b9b3f16ec1c209a60030ed73eb70f
otwgood <otwgood@naver.com>
2025-09-28 15:32:05 +0900

    .

---
+++ lib/week03/image_carousel/screen/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageControlloer();
+
+  
+}
